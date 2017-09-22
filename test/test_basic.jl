0372f90b0acf0e84b274dd707904d0b4e1a59186
Guy Aridor <garidor@gmail.com>
2017-09-21 22:05:40 -0400

    tests!

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -1,5 +1,12 @@
-using Queueing
+using Bandits
 using Base.Test
 
-# @test_approx_eq_eps a b c
-# test a == b
+
+sb = staticbandit([Normal(0,1), Normal(100, 1)])
+greedy_policy = GreedyPolicy()
+epsilon_greedy_policy = EpsilonGreedyPolicy(0.05)
+
+agent = BasicAgent(greedy_policy, sb, 0)
+stats = simulate(sb, agent, 100)
+@test_approx_eqs stats.regret 0 0.01
+@test_approx_eqs stats.action_attempts[2] 100 1.1 
