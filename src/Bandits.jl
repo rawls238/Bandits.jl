834370359592a7c3686d1cb1d9c48a1d7c9831ba
Kevin Squire <kevin.squire@gmail.com>
2020-01-02 23:41:19 -0800

    Remove unused import, undefined exports

--- src/Bandits.jl
+++ src/Bandits.jl
@@ -1,14 +1,11 @@
 module Bandits
-import Distributions
 import Base: >=, <=, +, /
 
 export
     Bandit, StaticBandit, staticbandit, pull, regret,
-    Arm, RandomArm,
     Policy, GreedyPolicy, EpsilonGreedyPolicy, ExploreThenExploit, UCB1, ThompsonSampling, choose,
     Agent, BasicAgent, BetaBernoulliAgent, NormalAgent, randomSample, choose, observe,
-    Action, generate_arbitrary_action,
-    BanditStats, simulate, aggregate_simulate
+    Action, BanditStats, simulate, aggregate_simulate
 
 include("utils.jl")
 include("actions.jl")
